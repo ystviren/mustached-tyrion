4fd6c80f6b02659a61a951b13f635c038e72c304
Cheung  Alan Ming-Yeung <cheun329@ug158.eecg>
2014-04-10 14:18:56 -0400

    Fix scripts

--- Lab4/main/src/ClientDriver.sh
+++ Lab4/main/src/ClientDriver.sh
@@ -2,4 +2,4 @@
 # ClientDriver.sh
 # $1 is zookeeper location
 
-java -classpath lib/zookeeper-3.3.2.jar:lib/log4j-1.2.15.jar ClientDriver $1
+java -classpath lib/zookeeper-3.3.2.jar:lib/log4j-1.2.15.jar:. ClientDriver $1
--- Lab4/main/src/FileServer.sh
+++ Lab4/main/src/FileServer.sh
@@ -2,4 +2,4 @@
 # JobTracker.sh
 # $1 listening port
 # $2 zookeeper location
-java -classpath lib/zookeeper-3.3.2.jar:lib/log4j-1.2.15.jar FileServer $1 $2
+java -classpath lib/zookeeper-3.3.2.jar:lib/log4j-1.2.15.jar:. FileServer $1 $2
